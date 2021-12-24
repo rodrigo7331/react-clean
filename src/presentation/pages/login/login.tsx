d7d7d7edbebbd2fa38d80e72c47ef9d41b49672d
rodrigo7331 <rodrigo7331@hotmail.com>
2021-12-24 14:12:19 -0300

    test: ensure login navigates to main page on success

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -5,7 +5,6 @@ import Context from '@/presentation/context/form/form-context'
 import { Validation } from '@/presentation/protocols/validation'
 import { Authentication } from '@/domain/usecases'
 import { Link, useHistory } from 'react-router-dom'
-import { homedir } from 'os'
 type Props = {
   validation?: Validation
   authentication?: Authentication
