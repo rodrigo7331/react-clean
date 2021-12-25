d5d18f71579803cddad98cd873367bca456984c9
rodrigo7331 <rodrigo7331@hotmail.com>
2021-12-25 15:37:54 -0300

    test: ensure ValidationComposite returns falsy if there is no error

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -3,16 +3,17 @@ import { fieldNameMock } from '@/validation/validators/test/mock-field'
 import faker from 'faker'
 import { MinLengthValidation } from './min-length-validation'
 
-const makeSut = (): MinLengthValidation => new MinLengthValidation(fieldNameMock,minLength)
-const minLength = 5
+const makeSut = (minLength: number): MinLengthValidation => new MinLengthValidation(fieldNameMock,minLength)
 describe('MinLengthValidation', () => {
   test('Should return error if value is invalid', () => {
-    const sut = makeSut()
+    const minLength = 5
+    const sut = makeSut(minLength)
     const error = sut.validate(faker.random.alphaNumeric(minLength - 1))
     expect(error).toEqual(new InvalidFieldError(fieldNameMock))
   })
   test('Should return falsy if valud is valid', () => {
-    const sut = makeSut()
+    const minLength = 5
+    const sut = makeSut(minLength)
     const error = sut.validate(faker.random.alphaNumeric(minLength))
     expect(error).toBeFalsy()
   })
--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -1,15 +1,15 @@
 import { FieldValidationSpy } from '@/validation/validators/test/mock-field-validation'
 import { ValidationComposite } from './validation-composite'
-
+import faker from 'faker'
 type SutTypes = {
   sut: ValidationComposite
   fieldValidationsSpy: FieldValidationSpy[]
 }
 
-const makeSut = (): SutTypes => {
+const makeSut = (fieldName: string): SutTypes => {
   const fieldValidationsSpy = [
-    new FieldValidationSpy('any_field'),
-    new FieldValidationSpy('any_field')
+    new FieldValidationSpy(fieldName),
+    new FieldValidationSpy(fieldName)
   ]
   const sut = new ValidationComposite(fieldValidationsSpy)
   return { sut, fieldValidationsSpy }
@@ -17,11 +17,21 @@ const makeSut = (): SutTypes => {
 
 describe('ValidationComposite', () => {
   test('Should retur error if any validation fails', () => {
-    const { sut,fieldValidationsSpy } = makeSut()
-    fieldValidationsSpy[0].error = new Error('first_error_message')
-    fieldValidationsSpy[1].error = new Error('any_error_message')
+    const fieldName = faker.database.column()
+    const { sut,fieldValidationsSpy } = makeSut(fieldName)
+    const errorMessage1 = faker.random.words()
+    const errorMessage2 = faker.random.words()
+    fieldValidationsSpy[0].error = new Error(errorMessage1)
+    fieldValidationsSpy[1].error = new Error(errorMessage2)
 
-    const error = sut.validate('any_field', 'any_value')
-    expect(error).toBe('first_error_message')
+    const error = sut.validate(fieldName, faker.random.words())
+    expect(error).toBe(errorMessage1)
+  })
+
+  test('Should return falsy if there is no error', () => {
+    const fieldName = faker.database.column()
+    const { sut } = makeSut(fieldName)
+    const error = sut.validate(fieldName, 'any_value')
+    expect(error).toBeFalsy()
   })
 })
