1a3af92ff9a92025c38a8f4f4da8f52a935bdab8
rodrigo7331 <rodrigo7331@hotmail.com>
2021-12-25 16:22:29 -0300

    test: ensure ValidationBuilder has a required helper

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,8 +1,14 @@
 import { RequiredFieldValidation, ValidationBuilder } from '..'
+import { EmailValidation } from '../email/email-validation'
 
 describe('ValidationBuilder', () => {
   test('Should return RequiredFieldValidation', () => {
     const validations = ValidationBuilder.field('any_field').required().build()
     expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+  })
+
+  test('Should return EmailValidation', () => {
+    const validations = ValidationBuilder.field('any_field').email().build()
+    expect(validations).toEqual([new EmailValidation('any_field')])
   })
 })
