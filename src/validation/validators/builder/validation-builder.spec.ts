5711806b03093369b562fc430ada44646318a5be
rodrigo7331 <rodrigo7331@hotmail.com>
2021-12-25 16:24:43 -0300

    test: ensure ValidationBuilder has a required helper

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,5 +1,6 @@
 import { RequiredFieldValidation, ValidationBuilder } from '..'
 import { EmailValidation } from '../email/email-validation'
+import { MinLengthValidation } from '../min-length/min-length-validation'
 
 describe('ValidationBuilder', () => {
   test('Should return RequiredFieldValidation', () => {
@@ -10,5 +11,10 @@ describe('ValidationBuilder', () => {
   test('Should return EmailValidation', () => {
     const validations = ValidationBuilder.field('any_field').email().build()
     expect(validations).toEqual([new EmailValidation('any_field')])
+  })
+
+  test('Should return MinLengthValidation', () => {
+    const validations = ValidationBuilder.field('any_field').min(3).build()
+    expect(validations).toEqual([new MinLengthValidation('any_field', 3)])
   })
 })
