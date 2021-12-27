3f4e7618cabc1b6263cafa20fdd30ea25b77f744
rodrigo7331 <rodrigo7331@hotmail.com>
2021-12-27 20:24:21 -0300

    test: ensure CompareFieldsValidation return falsy if validation succeeds

--- src/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -1,13 +1,36 @@
 import { InvalidFieldError } from '../../errors'
 import faker from 'faker'
 import { CompareFieldsValidation } from './compare-fields-validation'
-const field = faker.database.column()
-const value = faker.random.words()
-const makeSut = (valueToCompare: string = value): CompareFieldsValidation => (new CompareFieldsValidation(field, value))
+
+type SutParams = {
+  field: string
+  valueToCompare: string
+}
+
+let defaultSutParams: SutParams = {
+  field: faker.database.column(),
+  valueToCompare: faker.random.words()
+}
+
+const makeSut = (params?: SutParams): CompareFieldsValidation => (new CompareFieldsValidation(params.field, params.valueToCompare))
 describe('CompareFieldsValidation', () => {
+  afterEach(() => {
+    defaultSutParams = {
+      field: faker.database.column(),
+      valueToCompare: faker.random.words()
+    }
+  })
   test('Should return error if compare is invalid', () => {
-    const sut = makeSut(value)
+    const field = faker.database.column()
+    const sut = makeSut({ ...defaultSutParams, field: field })
     const error = sut.validate(faker.random.words())
     expect(error).toEqual(new InvalidFieldError(field))
+  })
+
+  test('Should return falsy if compare is valid', () => {
+    const value = faker.random.words()
+    const sut = makeSut({ ...defaultSutParams, valueToCompare: value })
+    const error = sut.validate(value)
+    expect(error).toBeFalsy()
   })
 })
