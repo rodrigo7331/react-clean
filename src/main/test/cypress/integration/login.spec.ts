0e5c70ab718e2393b4140048029c25a69f7ea5ab
rodrigo7331 <rodrigo7331@hotmail.com>
2021-12-29 20:55:55 -0300

    test: login tests +

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -4,6 +4,7 @@ const baseUrl: string = Cypress.config().baseUrl
 
 describe('Login', () => {
   beforeEach(() => {
+    cy.server()
     cy.visit('login')
   })
 
@@ -41,33 +42,85 @@ describe('Login', () => {
   })
 
   it('Should present error if invalid credentials are provided', () => {
+    cy.route({
+      status: 401,
+      method: 'POST',
+      url: /login/,
+      response: {
+        error: faker.random.words()
+      }
+    })
+
     cy.getByTestId('email').focus().type(faker.internet.email())
 
     cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
 
     cy.getByTestId('submit').click()
-    cy.getByTestId('error-wrap')
-      .getByTestId('spinner').should('exist')
-      .getByTestId('main-error').should('not.exist')
-      .getByTestId('spinner').should('not.exist')
+    cy.getByTestId('spinner').should('not.exist')
       .getByTestId('main-error').should('contain.text', 'Invalid Credentials')
 
     cy.url().should('eq', `${baseUrl}/login`)
   })
 
-  it('Shouldsave accessToken if valid credentials are provided', () => {
+  it('Should save accessToken if valid credentials are provided', () => {
+    cy.route({
+      status: 200,
+      method: 'POST',
+      url: /login/,
+      response: {
+        accessToken: faker.random.uuid()
+      }
+    })
+
     cy.getByTestId('email').focus().type('mango@gmail.com')
-
     cy.getByTestId('password').focus().type('12345')
-
     cy.getByTestId('submit').click()
-    cy.getByTestId('error-wrap')
-      .getByTestId('spinner').should('exist')
-      .getByTestId('main-error').should('not.exist')
+    cy.getByTestId('main-error').should('not.exist')
       .getByTestId('spinner').should('not.exist')
 
     cy.url().should('eq', `${baseUrl}/`)
 
     cy.window().then(window => assert.isOk(window.localStorage.getItem('accessToken')))
+  })
+
+  it('Should save accessToken if valid credentials are provided', () => {
+    cy.route({
+      status: 200,
+      method: 'POST',
+      url: /login/,
+      response: {
+        accessToken: faker.random.uuid()
+      }
+    })
+
+    cy.getByTestId('email').focus().type('mango@gmail.com')
+    cy.getByTestId('password').focus().type('12345')
+    cy.getByTestId('submit').click()
+    cy.getByTestId('main-error').should('not.exist')
+      .getByTestId('spinner').should('not.exist')
+
+    cy.url().should('eq', `${baseUrl}/`)
+
+    cy.window().then(window => assert.isOk(window.localStorage.getItem('accessToken')))
+  })
+
+  it('Should present UnexpectedError if invalid data is returned', () => {
+    cy.route({
+      status: 200,
+      method: 'POST',
+      url: /login/,
+      response: {
+        invalidProp: faker.random.uuid()
+      }
+    })
+
+    cy.getByTestId('email').focus().type('mango@gmail.com')
+    cy.getByTestId('password').focus().type('12345')
+    cy.getByTestId('submit').click()
+    cy.getByTestId('submit').click()
+    cy.getByTestId('spinner').should('not.exist')
+      .getByTestId('main-error').should('contain.text', 'Something went wrong :/')
+
+    cy.url().should('eq', `${baseUrl}/login`)
   })
 })
