fcd0ad65e1533d308ca445e9fbb3ad27ada8d856
rodrigo7331 <rodrigo7331@hotmail.com>
2021-12-28 23:37:53 -0300

    feat: add SignUpValidationFactory

---
+++ src/main/factories/pages/signup/signup-validation-factory.ts
@@ -0,0 +1,11 @@
+import { ValidationBuilder, ValidationComposite } from '@/validation/validators'
+
+export const makeSignUpValidation = (): ValidationComposite => {
+  const validationComposite = ValidationComposite.build([
+    ...ValidationBuilder.field('name').required().min(5).build(),
+    ...ValidationBuilder.field('email').required().email().build(),
+    ...ValidationBuilder.field('password').required().min(5).build(),
+    ...ValidationBuilder.field('passwordConfirmation').required().email().build()
+  ])
+  return validationComposite
+}
